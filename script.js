dcdf4b50684ad4ea5c66277422eabda3b918f996
kh4tib <34534018+kh4tib@users.noreply.github.com>
2018-03-24 14:15:41 +0200

    removed input js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const imageBackground = document.querySelector(".main__image");
 const colorSelector = document.querySelector(".content__colors");
 
+colorSelector.onchange = function() {changeBackground(this)};
+
 function changeBackground(x) {
+    console.log(x.value);
     imageBackground.style.background = x.value;
 }
-
